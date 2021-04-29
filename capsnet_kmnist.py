b27325202695f3c00c7be3d17c8f60784e9f791e
Léopold Maillard <56095779+leopoldmaillard@users.noreply.github.com>
2021-04-30 00:03:20 +0200

    Update capsnet_kmnist.py

--- capsnet_kmnist.py
+++ capsnet_kmnist.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+print(tf.__version__)
+
 X_train = np.load('data/kmnist-train-imgs.npz')['arr_0']
 Y_train = np.load('data/kmnist-train-labels.npz')['arr_0']
 
